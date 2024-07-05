66a3f6e48e26169634c90e1207c8cfc0c0acbf1a
zouhao <zouhao@joyinngames.com>
2024-07-05 17:52:00 +0800

    feat: 修改StartupGame方法

--- Client/Project/Assets/Scripts/HotUpdate/Runtime/StartupGame.cs
+++ Client/Project/Assets/Scripts/HotUpdate/Runtime/StartupGame.cs
@@ -4,8 +4,9 @@ using Joy.MVVM;
 using Joy.Asset;
 using Joy;
 using Cysharp.Threading.Tasks;
+using HQ.Launcher;
 
-public class StartupGame : MonoBehaviour
+public class StartupGame : Launcher
 {
     void Awake()
     {
@@ -14,15 +15,16 @@ public class StartupGame : MonoBehaviour
 
     async void Startup()
     {
-        TickManager.Init(1);
+        TickManager.Init();
+
         await AssetManager.Instance.InitAsync();
         await UIManager.Instance.InitAsync();
-
+        await LaunchAsync();
         var view = Joy.MVVM.UIManager.Instance.OpenWindow<LoadingPage>(WindowType.Page);
     }
 
     void Update()
     {
-        TickManager.OnTick(1);
+        TickManager.OnTick();
     }
 }
